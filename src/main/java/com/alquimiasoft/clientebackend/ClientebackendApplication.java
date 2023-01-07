28e0a093f49634929bbb625dc67892c42e344f6f
psidrovo <paulsidrovob@gmail.com>
2023-01-07 12:16:53 -0500

    Inicio del proyecto

---
+++ src/main/java/com/alquimiasoft/clientebackend/ClientebackendApplication.java
@@ -0,0 +1,13 @@
+package com.alquimiasoft.clientebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alquimiasoft/clientebackend/ClientebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquimiasoft.clientebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
